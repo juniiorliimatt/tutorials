194701a0a3791fd24a62fa856bbbec8c48b149ba
Junior Lima <juniiordev@gmail.com>
2021-07-16 19:17:14 -0300

    add exceptions

---
+++ src/main/java/br/com/tutorials/domain/services/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,7 @@
+package br.com.tutorials.domain.services.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException{
+  public ResourceNotFoundException(Object id){
+    super("Resource not found. ID "+id);
+  }
+}
