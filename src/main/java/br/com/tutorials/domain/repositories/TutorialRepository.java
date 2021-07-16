c376107d965e8f965aed4c7783088aacd4456108
Junior Lima <juniiorliimatt@gmail.com>
2021-07-16 16:19:57 -0300

    add repository

---
+++ src/main/java/br/com/tutorials/domain/repositories/TutorialRepository.java
@@ -0,0 +1,12 @@
+package br.com.tutorials.domain.repositories;
+
+import br.com.tutorials.domain.models.Tutorial;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface TutorialRepository extends JpaRepository<Tutorial, Long>{
+  List<Tutorial> findByPublished(Boolean published);
+
+  List<Tutorial> findByTitleContaining(String title);
+}
