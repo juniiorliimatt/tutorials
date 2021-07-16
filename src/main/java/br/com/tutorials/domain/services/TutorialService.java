6222a27f491655743cbb57919eb75a025bbdf71b
Junior Lima <juniiordev@gmail.com>
2021-07-16 19:14:49 -0300

    add methods on service findbyid and update

--- src/main/java/br/com/tutorials/domain/services/TutorialService.java
+++ src/main/java/br/com/tutorials/domain/services/TutorialService.java
@@ -2,17 +2,19 @@ package br.com.tutorials.domain.services;
 
 import br.com.tutorials.domain.models.Tutorial;
 import br.com.tutorials.domain.repositories.TutorialRepository;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
 import java.io.Serializable;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class TutorialService implements Serializable{
 
-  private TutorialRepository repository;
+  private final TutorialRepository repository;
 
   @Autowired
   TutorialService(TutorialRepository repository){
@@ -26,6 +28,21 @@ public class TutorialService implements Serializable{
 
   @Transactional
   public List<Tutorial> findByTitle(String title){
-    return repository.findByTitleContaining(title);
+    return repository.findByTitleStartingWith(title);
+  }
+
+  public Optional<Tutorial> findById(Long id){
+    return repository.findById(id);
+  }
+
+  @Transactional
+  public Tutorial save(Tutorial tutorial){
+    return repository.save(tutorial);
+  }
+
+  public Tutorial update(Long id, Tutorial tutorial){
+    var update=repository.getById(id);
+    BeanUtils.copyProperties(tutorial, update);
+    return repository.save(update);
   }
 }
